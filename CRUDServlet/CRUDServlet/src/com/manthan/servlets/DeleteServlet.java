71f9e09cc22811e5b93ddfda514fbdd3fd09c1d5
pranathi2495 <61830358+pranathi2495@users.noreply.github.com>
2020-11-23 18:21:20 +0530

    uploaded file

---
+++ CRUDServlet/CRUDServlet/src/com/manthan/servlets/DeleteServlet.java
@@ -0,0 +1,19 @@
+package com.manthan.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/DeleteServlet")  
+public class DeleteServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)   
+			throws ServletException, IOException {  
+		String sid=request.getParameter("id");  
+		int id=Integer.parseInt(sid);  
+		EmpDao.delete(id);  
+		response.sendRedirect("ViewServlet");  
+	}  
+}
---
+++ HelloWorldServlets/src/com/manthan/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.manthan;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/helloworld")
+
+public class HelloWorld extends HttpServlet{
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// Set response content type
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + "Hello World" + "</h1>");
+	}
+
+
+}
